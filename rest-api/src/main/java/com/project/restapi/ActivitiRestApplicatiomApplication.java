0243c31203449a257eb727b76d9a4cecdd8ceaa2
ugur ciftci <ugurprog@gmail.com>
2021-02-02 23:21:11 +0300

    create spring boot project

---
+++ rest-api/src/main/java/com/project/restapi/ActivitiRestApplicatiomApplication.java
@@ -0,0 +1,13 @@
+package com.project.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiRestApplicatiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiRestApplicatiomApplication.class, args);
+	}
+
+}
