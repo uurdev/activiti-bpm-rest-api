fe0b8d9b37aef5fa16a792d2bae79ee2be39b129
Uğur Çiftçi <ugurciftci@beyazweb.com.tr>
2021-02-05 11:18:32 +0300

    dto service config layer added

--- rest-api/src/main/java/com/project/restapi/configuration/ActivitiBpmEngineConfiguration.java
+++ rest-api/src/main/java/com/project/restapi/configuration/ActivitiBpmEngineConfiguration.java
@@ -1,76 +1,83 @@
 package com.project.restapi.configuration;
 
-import org.activiti.engine.*;
+import javax.sql.DataSource;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.activiti.spring.SpringProcessEngineConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-
 @Configuration
 @ComponentScan
-@PropertySource({"classpath:application.properties"})
 public class ActivitiBpmEngineConfiguration {
 
+	@Bean
+	public DataSource postgreDatabaseConnection() {
+		return DataSourceBuilder.create().url("jdbc:postgresql://localhost:5432/activitirest").username("postgres").password("postgres").driverClassName("org.postgresql.Driver").build();
+	}
 
-    @Bean
-    public DataSource databaseConnection() {
-        return DataSourceBuilder.create().url("jdbc:postgresql://localhost:5432/activitirest").username("postgres")
-                .password("postgres").driverClassName("org.postgresql.Driver").build();
-    }
+	@Bean
+	DataSource h2DatabaseConnection() {
+		return DataSourceBuilder.create().url("jdbc:h2:mem:activitirest").username("sa").password("password").driverClassName("org.h2.Driver").build();
+	}
 
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(databaseConnection());
-    }
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(h2DatabaseConnection());
+	}
 
-    @Bean
-    public ProcessEngineFactoryBean processEngine() {
-        ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
-        factoryBean.setProcessEngineConfiguration(processEngineConfiguration());
-        return factoryBean;
-    }
+	@Bean
+	public ProcessEngineFactoryBean processEngine() {
+		ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
+		factoryBean.setProcessEngineConfiguration(processEngineConfiguration());
+		return factoryBean;
+	}
 
-    @Bean
-    public SpringProcessEngineConfiguration processEngineConfiguration() {
-        SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
-        config.setDataSource(databaseConnection());
-        config.setTransactionManager(transactionManager());
-        config.setDatabaseSchemaUpdate("update");
-        config.setHistory("audit");
-        config.setAsyncExecutorActivate(true);
-        return config;
-    }
+	@Bean
+	public SpringProcessEngineConfiguration processEngineConfiguration() {
+		SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
+		config.setDataSource(h2DatabaseConnection());
+		config.setTransactionManager(transactionManager());
+//		config.setDatabaseSchemaUpdate("update");
+		config.setDatabaseSchemaUpdate("create-drop");
+		config.setHistory("audit");
+		config.setAsyncExecutorActivate(true);
+		return config;
+	}
 
+	@Bean
+	public RepositoryService repositoryService(ProcessEngine processEngine) {
+		return processEngine.getRepositoryService();
+	}
 
-    @Bean
-    public RepositoryService repositoryService(ProcessEngine processEngine) {
-        return processEngine.getRepositoryService();
-    }
+	@Bean
+	public RuntimeService runtimeService(ProcessEngine processEngine) {
+		return processEngine.getRuntimeService();
+	}
 
-    @Bean
-    public RuntimeService runtimeService(ProcessEngine processEngine) {
-        return processEngine.getRuntimeService();
-    }
+	@Bean
+	public TaskService taskService(ProcessEngine processEngine) {
+		return processEngine.getTaskService();
+	}
 
-    @Bean
-    public TaskService taskService(ProcessEngine processEngine) {
-        return processEngine.getTaskService();
-    }
+	@Bean
+	public IdentityService identityService(ProcessEngine processEngine) {
+		return processEngine.getIdentityService();
+	}
 
-    @Bean
-    public IdentityService identityService(ProcessEngine processEngine) {
-        return processEngine.getIdentityService();
-    }
-
-    @Bean
-    public ManagementService managementService(ProcessEngine processEngine) {
-        return processEngine.getManagementService();
-    }
+	@Bean
+	public ManagementService managementService(ProcessEngine processEngine) {
+		return processEngine.getManagementService();
+	}
 
 }
---
+++ rest-api/src/test/java/com/project/restapi/service/BpmUserServiceTest.java
@@ -0,0 +1,14 @@
+package com.project.restapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BpmUserServiceTest {
+
+	@Test
+	void createUser() {
+
+	}
+
+}
